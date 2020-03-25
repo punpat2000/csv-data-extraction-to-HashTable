14af5f52c6bede4f99ce9020c65fd966bb9e9642
punpat2000 <saint12302@gmail.com>
2020-03-25 17:29:18 +0700

    🥰

--- src/CovidData.java
+++ src/CovidData.java
@@ -9,7 +9,6 @@ public class CovidData {
 		data = new Hashtable<String,Integer>();
 		try {
 			Scanner in = new Scanner(new File(filename));
-			// Add your code here 
 			while(in.hasNext()) {
 				String[] tokens = in.nextLine().split(",");
 				this.data.put(tokens[0], Integer.valueOf(tokens[1]));
@@ -20,7 +19,6 @@ public class CovidData {
 		
 	}
 	public int find(String d) {
-		// Add your code here 
 		var data = this.data.get(d);
 		return data == null ? -1 : (int) data;
 	}
